511f1e4979b16f8e1f39a7c23f76917096d14e99
Tom Pelaia <tapsns@users.sourceforge.net>
2011-02-18 13:21:21 -0500

    add the XAL tools fit package

---
+++ core/src/tools/fit/PolyLeastsquaresException.java
@@ -0,0 +1,13 @@
+/*
+ * PolyLeatsSquaresException
+ */
+
+package xal.tools.fit;
+
+public class PolyLeastsquaresException
+    extends Exception
+{
+    PolyLeastsquaresException(String msg){
+	super(msg);
+    }
+}
