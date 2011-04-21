d86a6baddd7a4e3fe83766270346b2f44e66ecd0
Christopher K. Allen <allenck@ornl.gov>
2011-04-21 07:52:18 -0400

    Changed element to SNS version which is more up to date

--- common/core/xal/model/elem/IdealMagSextupole.java
+++ common/core/xal/model/elem/IdealMagSextupole.java
@@ -11,29 +11,23 @@ package xal.model.elem;
 import xal.model.IProbe;
 import xal.model.PropagationException;
 import xal.tools.beam.PhaseMap;
+import xal.tools.beam.PhaseMatrix;
 
 
 
-/** 
- * Sextupole magnets element
- *  
- *  Created by Thomas Pelaia on 10/12/05.
- *  Copyright 2005 Oak Ridge National Lab. All rights reserved.
- */
-public class IdealMagSextupole extends ThinElement {
+/** Sextupole magnets element */
+public class IdealMagSextupole extends ThickElement {
     /** type string identifier for all sextople elements */
     public static final String s_strType = "IdealMagSextupole";
-    
-    /** identity phase map */
-    private static final PhaseMap IDENTITY_PHASE_MAP = PhaseMap.identity();
 	
 	
 	/** 
-	 *  Constructor
-	 *  @param  strId   identifier of this object
+	 * Constructor
+	 * @param  strID identifier of this object
+	 * @param length magnetic length of the magnet
 	 */
-    public IdealMagSextupole( final String strId ) {
-        super( s_strType, strId );
+    public IdealMagSextupole( final String strID, final double length ) {
+        super( s_strType, strID, length );
     }
     
 	
@@ -44,31 +38,45 @@ public class IdealMagSextupole extends ThinElement {
     
     
     /**
-	 * Returns the time taken for the probe to propagate through element.
-     *  @param  probe   propagating probe
-     *  @return         the value zero 
+	 * Determine the time taken for the probe to propagate through element.
+	 * @param probe probe at the entrance to this element
+	 * @param length of the magnet
+     * @return         the value zero 
      */
-    @Override
-    public double elapsedTime( final IProbe probe )  {
-        return 0.0;
+    public double elapsedTime( final IProbe probe, final double length )  {
+        return super.compDriftingTime( probe, length );
     }
 
     
     /**
-	 *  Returns energy gain which is zero.
-     *  @param  probe   dummy argument
-     *  @return         returns zero
+	 * Determine energy gain which is zero.
+	 * @param probe probe at the entrance to this element
+	 * @param length of the magnet
+     * @return         returns zero
      */
-    @Override
-    public double energyGain(IProbe probe) { return 0.0;  }
+    public double energyGain( final IProbe probe, final double length ) { return 0.0;  }
 	
     
     /**  
-	 *  Returns the transfer map of this element which is the identity.
-	 *  @param  probe   dummy argument
-	 *  @return         the identity phase map
-	 *  @exception  PropagationException  this should not occur
+	 * Determine the transfer map of this element which is simply treated as a drift.
+	 * @param probe probe at the entrance to this element
+	 * @param length of the magnet
+	 * @return transfer map
+	 * @exception  PropagationException  this should not occur
 	 */
-    @Override
-    protected PhaseMap transferMap(IProbe probe) throws PropagationException { return IDENTITY_PHASE_MAP; }
+    public PhaseMap transferMap( final IProbe probe, final double length ) throws PropagationException { 
+        // Build transfer matrix
+        PhaseMatrix  matPhi  = new PhaseMatrix();
+        
+		// one dimensional drift matrix
+        double mat0[][] = new double [][] { { 1.0, length }, { 0.0, 1.0 } };
+		
+		// set the block diagonals to the one dimensional drift matricies
+        matPhi.setSubMatrix( 0, 1, 0, 1, mat0 );
+        matPhi.setSubMatrix( 2, 3, 2 ,3, mat0 );
+        matPhi.setSubMatrix( 4, 5, 4, 5, mat0 );
+        matPhi.setElem( 6, 6, 1.0 );	// inhomogeneous term is always 1.0
+		
+        return new PhaseMap( matPhi );
+	}
 }
