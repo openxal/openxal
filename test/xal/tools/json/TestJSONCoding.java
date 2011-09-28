b4958a648eb49f844392179ea48ba142191658e5
Thomas Pelaia II <pelaiata@ornl.gov>
2011-09-28 11:58:56 -0400

    -n Add JSON coding tests for lists and maps

--- test/xal/tools/json/TestJSONCoding.java
+++ test/xal/tools/json/TestJSONCoding.java
@@ -45,6 +45,40 @@ public class TestJSONCoding {
     }
     
     
+    @Test
+    public void testArrayCoding() {
+        assertEquality( "[1.2, -17.6, 5.4E23, 1.2E-6]", JSONCoder.encode( new Double[] { 1.2, -17.6, 5.4E23, 1.2E-6 } ) );
+        checkCodingEquality( "[\"Hello\", \"World\"]", new String[] { "Hello", "World" } );
+        checkCodingEquality( "[\"Testing\", \"\\\"internal\\\" string.\"]", new String[] { "Testing", "\"internal\" string." } );
+    }
+    
+    
+    @Test
+    public void testListCoding() {
+        final List testList = new ArrayList();
+        testList.add( "Hello" );
+        testList.add( "World" );
+        testList.add( 1.5 );
+        testList.add( 2.7 );
+        testList.add( -324.2 );
+        assertEquality( "[\"Hello\", \"World\", 1.5, 2.7, -324.2]", JSONCoder.encode( testList ) );
+    }
+    
+    
+    @Test
+    public void testMapCoding() {
+        final Map testMap = new TreeMap();  // need to guarantee ordering so we can test against our control
+        testMap.put( "message", "Hello, World" );
+        testMap.put( "value", 5.4 );
+        testMap.put( "cities", new String[] { "Knoxville", "Oak Ridge" } );
+        // make sure the control coding lists keys in alphabetical ordering (corresponding to TreeMap)
+        final String controlCoding = "{\"cities\": [\"Knoxville\", \"Oak Ridge\"], \"message\": \"Hello, World\", \"value\": 5.4}";
+//        System.out.println( controlCoding );
+//        System.out.println( JSONCoder.encode( testMap ) );
+        assertEquality( controlCoding, JSONCoder.encode( testMap ) );
+    }
+    
+    
     /** check whether the coder can encode values */
     static private <DataType> void checkCodingEquality( final DataType value ) {
         final String controlCoding = String.valueOf( value );
