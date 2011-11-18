6aae356853160f6ed589f2c79228358803f6496f
Thomas Pelaia II <pelaiata@ornl.gov>
2011-11-18 15:26:57 -0500

    Implement the Optics Switcher application which allows the user to specify the default optics.

---
+++ apps/opticsswitcher/xal/app/opticsswitcher/SwitcherDocument.java
@@ -0,0 +1,63 @@
+/*
+ * SwitcherDocument.java
+ *
+ * Created on November 8, 2011, 3:28 PM
+ */
+
+package xal.app.opticsswitcher;
+
+import java.net.*;
+import java.io.*;
+import java.awt.Color;
+import javax.swing.*;
+import javax.swing.JToggleButton.ToggleButtonModel;
+import javax.swing.text.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.util.logging.*;
+import java.util.Vector;
+
+import xal.application.*;
+import xal.tools.bricks.WindowReference;
+import xal.tools.plot.*;
+import xal.smf.data.OpticsSwitcher;
+
+
+/**
+ * SwitcherDocument is a custom XalDocument for this application.
+ * @author  t6p
+ */
+public class SwitcherDocument extends XalDocument {
+    /** Create a new empty document */
+    public SwitcherDocument() {
+        this( null );
+    }
+    
+    
+    /** 
+     * Create a new document loaded from the URL file 
+     * @param url The URL of the file to load into the new document.
+     */
+    public SwitcherDocument( final URL url ) {
+        setSource( url );
+    }
+    
+    
+    /**
+     * Make a main window by instantiating the my custom window.  Set the text 
+     * pane to use the textDocument variable as its document.
+     */
+    public void makeMainWindow() {
+        mainWindow = new DefaultXalWindow( this, false );
+        final OpticsSwitcher opticsSwitcher = OpticsSwitcher.getInstanceForHostedFrame();
+        mainWindow.getContentPane().add( opticsSwitcher.getView() );
+        mainWindow.pack();
+	}
+    
+    
+    /**
+     * Save the document to the specified URL.
+     * @param url The URL to which the document should be saved.
+     */
+    public void saveDocumentAs( final URL url ) {}
+}
