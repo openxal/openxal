c33eb0210a41518bb2564a4fa0052f2060c79762
Thomas Pelaia II <pelaiata@ornl.gov>
2012-09-06 15:04:07 -0400

    Use a dispatch queue to manage access to the remote app records.

--- apps/launcher/xal/app/launcher/MonitorModel.java
+++ apps/launcher/xal/app/launcher/MonitorModel.java
@@ -9,14 +9,19 @@
 package xal.app.launcher;
 
 import java.util.*;
+import java.util.concurrent.Callable;
 
 import xal.tools.services.*;
+import xal.tools.dispatch.DispatchQueue;
 import xal.application.ApplicationStatus;
 import xal.tools.messaging.MessageCenter;
 
 
 /** MonitorModel */
 public class MonitorModel extends java.lang.Object {
+	/** queue to manage access to the remote apps list and map */
+	final private DispatchQueue REMOTE_APPS_ACCESS_QUEUE;
+
 	/** list of remote applications */
 	final private List<RemoteAppRecord> REMOTE_APPS;
 
@@ -30,7 +35,8 @@ public class MonitorModel extends java.lang.Object {
 	/** Constructor */
     public MonitorModel() {
 		EVENT_PROXY = MessageCenter.defaultCenter().registerSource( this, MonitorModelListener.class );
-		
+
+		REMOTE_APPS_ACCESS_QUEUE = DispatchQueue.createConcurrentQueue( "Remote Apps Access" );
 		REMOTE_APPS = new ArrayList<RemoteAppRecord>();
 		REMOTE_APPS_TABLE = new HashMap<String,RemoteAppRecord>();
 
@@ -40,9 +46,11 @@ public class MonitorModel extends java.lang.Object {
 
 	/** Get the list of remote apps */
 	public List<RemoteAppRecord> getRemoteApps() {
-		synchronized( REMOTE_APPS ) {
-			return new ArrayList<RemoteAppRecord>( REMOTE_APPS );
-		}
+		return REMOTE_APPS_ACCESS_QUEUE.dispatchSync( new Callable<List<RemoteAppRecord>>() {
+			public List<RemoteAppRecord> call() {
+				return new ArrayList<RemoteAppRecord>( REMOTE_APPS );
+			}
+		});
 	}
 
 
@@ -69,18 +77,17 @@ public class MonitorModel extends java.lang.Object {
 			 */
 			public void serviceAdded( final ServiceDirectory directory, final ServiceRef serviceRef ) {
 				final ApplicationStatus proxy = directory.getProxy( ApplicationStatus.class, serviceRef );
-				final RemoteAppRecord remoteRecord = new RemoteAppRecord( proxy );
+				final RemoteAppRecord remoteRecord = new RemoteAppRecord( proxy, serviceRef.getHostAddress() );
 				final String serviceID = serviceRef.getRawName();
 				
 				System.out.println( "Found remote application with ID: " + serviceID );
 
-				synchronized( REMOTE_APPS_TABLE ) {
-					REMOTE_APPS_TABLE.put( serviceID, remoteRecord );
-				}
-
-				synchronized( REMOTE_APPS ) {
-					REMOTE_APPS.add( remoteRecord );
-				}
+				REMOTE_APPS_ACCESS_QUEUE.dispatchBarrierAsync( new Runnable() {
+					public void run() {
+						REMOTE_APPS_TABLE.put( serviceID, remoteRecord );
+						REMOTE_APPS.add( remoteRecord );
+					}
+				});
 
 				EVENT_PROXY.remoteAppsChanged( MonitorModel.this, getRemoteApps() );
 			}
@@ -93,16 +100,15 @@ public class MonitorModel extends java.lang.Object {
 			 * @param name The name of the removed service.
 			 */
 			public void serviceRemoved( final ServiceDirectory directory, final String type, final String name ) {
-				System.out.println( "Removing application with name: " + name );
-				
-				RemoteAppRecord remoteRecord = null;
-				synchronized( REMOTE_APPS_TABLE ) {
-					remoteRecord = REMOTE_APPS_TABLE.get( name );
-					REMOTE_APPS_TABLE.remove( name );
-				}
-				synchronized( REMOTE_APPS ) {
-					REMOTE_APPS.remove( remoteRecord );
-				}
+				System.out.println( "Removing application with name: " + name );				
+
+				REMOTE_APPS_ACCESS_QUEUE.dispatchBarrierAsync( new Runnable() {
+					public void run() {
+						final RemoteAppRecord remoteRecord = REMOTE_APPS_TABLE.get( name );
+						REMOTE_APPS_TABLE.remove( name );
+						REMOTE_APPS.remove( remoteRecord );
+					}
+				});
 				
 				EVENT_PROXY.remoteAppsChanged( MonitorModel.this, getRemoteApps() );
 			}
