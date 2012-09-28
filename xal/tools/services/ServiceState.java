05ca6602a2172e114e13864d902016d11afdd472
Thomas Pelaia II <pelaiata@ornl.gov>
2012-09-28 16:23:25 -0400

    -n The remote proxy now also is a proxy to a subset of methods in the local client handler by implementing the ServiceState interface.

---
+++ xal/tools/services/ServiceState.java
@@ -0,0 +1,37 @@
+//
+// ServiceState.java
+// 
+//
+// Created by Tom Pelaia on 9/28/12
+// Copyright 2012 Oak Ridge National Lab. All rights reserved.
+//
+
+package xal.tools.services;
+
+
+/** Provides methods for getting information and controlling the client side of the service through the proxy */
+public interface ServiceState {
+    /**
+     * Get the name of the remote service.
+     * @return The name of the remote service.
+     */
+    public String getServiceName();
+
+	
+    /**
+     * Get the host name of the remote service.
+     * @return The host name of the remote service.
+     */
+    public String getServiceHost();
+
+	
+    /**
+     * Get the port of the remote service.
+     * @return The port of the remote service.
+     */
+    public int getServicePort();
+
+
+	/** dispose of this proxy's resources */
+	public void disposeServiceResources();
+}
