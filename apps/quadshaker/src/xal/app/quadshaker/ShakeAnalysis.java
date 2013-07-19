7bbbca9404807da6767e6a9df2205942578d962f
Hale <aez@mac83116.ornl.gov>
2013-07-19 08:30:10 -0400

    Changed to use AlgorithmFactory to create EnvelopeTracker.

--- apps/quadshaker/src/xal/app/quadshaker/ShakeAnalysis.java
+++ apps/quadshaker/src/xal/app/quadshaker/ShakeAnalysis.java
@@ -35,6 +35,7 @@ import xal.model.probe.traj.*;
 import xal.sim.sync.*;
 //import xal.model.probe.resp.traj.*;
 import xal.tools.beam.*;
+import xal.model.alg.EnvelopeTracker;
 
 /**
  *  Description of the Class
@@ -42,64 +43,64 @@ import xal.tools.beam.*;
  *@author     shishlo
  */
 public class ShakeAnalysis {
-
+    
 	//main panel
 	private JPanel shakeAnalysisMainPanel = new JPanel();
-
+    
 	//Tables and List models for BPMs and Quads
 	private QuadsTable quadsTableModel = null;
 	private BPMsTable bpmsTableModel = null;
-
+    
 	//Orbit corrector controller
 	//The data will be initialized after analysis
 	private OrbitCorrector orbitCorrector = null;
-
+    
 	//the graphs panel to show the scan results
 	private FunctionGraphsJPanel graphXPanel = new FunctionGraphsJPanel();
 	private FunctionGraphsJPanel graphYPanel = new FunctionGraphsJPanel();
-
+    
 	private HashMap<BPM_Element, BasicGraphData> graphXDataMap = new HashMap<BPM_Element, BasicGraphData>();
 	private HashMap<BPM_Element, BasicGraphData> graphYDataMap = new HashMap<BPM_Element, BasicGraphData>();
-
+    
 	private BasicGraphData graphPosXData = new BasicGraphData();
 	private BasicGraphData graphPosYData = new BasicGraphData();
-
+    
 	//left panel elements
 	private TitledBorder quadTableBorder = null;
 	private TitledBorder bpmTableBorder = null;
-
+    
 	private JTable quadsTable = new JTable();
 	private JTable bpmsTable = new JTable();
-
+    
 	//Controls elements
 	private TitledBorder analysisControlBorder = null;
-
+    
 	private JRadioButton usePVLoggerButton = new JRadioButton("Use PV Logger Data with ID: ", false);
 	private DoubleInputTextField pvLoggerIdTextField = new DoubleInputTextField(8);
-
+    
 	private JButton analysisButton = new JButton("START ANALYSIS");
 	private JButton rePlotButton = new JButton("RE-PLOT DATA");
-
+    
 	//validates measured offsets
 	private OffSetValidationController validationCntr = new OffSetValidationController();
-
+    
 	//accelerator related objects
 	private AcceleratorSeqCombo accSeq = null;
 	private HashMap<String, BPM> bpmIdMap = new HashMap<String, BPM>();
 	private HashMap<String, Quadrupole> quadIdMap = new HashMap<String, Quadrupole>();
-
+    
 	//message text field. It is actually message text field from Window
 	private JTextField messageTextLocal = new JTextField();
-
+    
 	/**
 	 *  Constructor for the ShakeAnalysis object
 	 */
 	public ShakeAnalysis() {
-
+        
 		Border border = BorderFactory.createEtchedBorder();
-
+        
 		shakeAnalysisMainPanel.setLayout(new BorderLayout());
-
+        
 		//define graph panel's properties
 		SimpleChartPopupMenu.addPopupMenuTo(graphXPanel);
 		graphXPanel.setOffScreenImageDrawing(true);
@@ -110,7 +111,7 @@ public class ShakeAnalysis {
 		graphXPanel.setChooseModeButtonVisible(true);
 		graphXPanel.setLegendBackground(Color.white);
 		graphXPanel.setSmartGL(true);
-
+        
 		SimpleChartPopupMenu.addPopupMenuTo(graphYPanel);
 		graphYPanel.setOffScreenImageDrawing(true);
 		graphYPanel.setName("Quads' Y Offsets");
@@ -120,7 +121,7 @@ public class ShakeAnalysis {
 		graphYPanel.setChooseModeButtonVisible(true);
 		graphYPanel.setLegendBackground(Color.white);
 		graphYPanel.setSmartGL(true);
-
+        
 		graphPosXData.setDrawLinesOn(true);
 		graphPosXData.setDrawPointsOn(true);
 		graphPosXData.setGraphColor(Color.blue);
@@ -128,7 +129,7 @@ public class ShakeAnalysis {
 		graphPosXData.setLineThick(3);
 		graphPosXData.setImmediateContainerUpdate(false);
 		graphPosXData.setGraphProperty(graphXPanel.getLegendKeyString(), "Average Trajectory X");
-
+        
 		graphPosYData.setDrawLinesOn(true);
 		graphPosYData.setDrawPointsOn(true);
 		graphPosYData.setGraphColor(Color.red);
@@ -136,94 +137,94 @@ public class ShakeAnalysis {
 		graphPosYData.setLineThick(3);
 		graphPosYData.setImmediateContainerUpdate(false);
 		graphPosYData.setGraphProperty(graphXPanel.getLegendKeyString(), "Average Trajectory Y");
-
+        
 		graphXPanel.addGraphData(graphPosXData);
 		graphYPanel.addGraphData(graphPosYData);
-
+        
 		JPanel panel_innG = new JPanel(new GridLayout(2, 1, 0, 0));
 		panel_innG.add(graphXPanel);
 		panel_innG.add(graphYPanel);
-
+        
 		JPanel panel_G = new JPanel(new BorderLayout());
 		panel_G.setBorder(border);
 		panel_G.add(panel_innG, BorderLayout.CENTER);
 		shakeAnalysisMainPanel.add(panel_G, BorderLayout.CENTER);
-
+        
 		quadTableBorder = BorderFactory.createTitledBorder(border, "quads table");
 		bpmTableBorder = BorderFactory.createTitledBorder(border, "bpms table");
 		analysisControlBorder = BorderFactory.createTitledBorder(border, "analysis control");
-
+        
 		JPanel panel_PVLog = new JPanel(new GridLayout(1, 2, 1, 1));
 		panel_PVLog.setBorder(border);
 		panel_PVLog.add(usePVLoggerButton);
 		panel_PVLog.add(pvLoggerIdTextField);
 		pvLoggerIdTextField.setEnabled(usePVLoggerButton.isSelected());
-
+        
 		JPanel panel_StartAnal = new JPanel(new FlowLayout(FlowLayout.LEFT, 1, 1));
 		panel_StartAnal.add(analysisButton);
 		panel_StartAnal.add(rePlotButton);
-
+        
 		JPanel panel_Cntrl = new JPanel(new BorderLayout());
 		panel_Cntrl.add(panel_PVLog, BorderLayout.NORTH);
 		panel_Cntrl.add(panel_StartAnal, BorderLayout.CENTER);
 		panel_Cntrl.add(validationCntr.getPanel(), BorderLayout.SOUTH);
 		panel_Cntrl.setBorder(analysisControlBorder);
-
+        
 		JPanel panel_lT = new JPanel(new GridLayout(2, 1, 1, 1));
-
+        
 		JPanel panel_l = new JPanel(new BorderLayout());
 		panel_l.add(panel_Cntrl, BorderLayout.NORTH);
 		panel_l.add(panel_lT, BorderLayout.CENTER);
-
+        
 		shakeAnalysisMainPanel.add(panel_l, BorderLayout.WEST);
-
+        
 		JPanel panel_lT0 = new JPanel(new BorderLayout());
 		JPanel panel_lT1 = new JPanel(new BorderLayout());
-
+        
 		panel_lT0.setBorder(quadTableBorder);
 		panel_lT1.setBorder(bpmTableBorder);
-
+        
 		panel_lT.add(panel_lT0);
 		panel_lT.add(panel_lT1);
-
+        
 		JScrollPane scrollPane0 = new JScrollPane(quadsTable);
 		JScrollPane scrollPane1 = new JScrollPane(bpmsTable);
 		panel_lT0.add(scrollPane0);
 		panel_lT1.add(scrollPane1);
-
+        
 		quadsTable.setPreferredScrollableViewportSize(new Dimension(1, 1));
 		bpmsTable.setPreferredScrollableViewportSize(new Dimension(1, 1));
-
+        
 		usePVLoggerButton.addActionListener(
-			new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					if(usePVLoggerButton.isSelected()) {
-						pvLoggerIdTextField.setEnabled(true);
-					} else {
-						pvLoggerIdTextField.setEnabled(false);
-					}
-				}
-			});
-
+                                            new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if(usePVLoggerButton.isSelected()) {
+                    pvLoggerIdTextField.setEnabled(true);
+                } else {
+                    pvLoggerIdTextField.setEnabled(false);
+                }
+            }
+        });
+        
 		analysisButton.addActionListener(
-			new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					messageTextLocal.setText(null);
-					performAnalysis();
-					addPointsToGraph();
-				}
-			});
-
+                                         new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                messageTextLocal.setText(null);
+                performAnalysis();
+                addPointsToGraph();
+            }
+        });
+        
 		rePlotButton.addActionListener(
-			new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					messageTextLocal.setText(null);
-					addPointsToGraph();
-				}
-			});
+                                       new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                messageTextLocal.setText(null);
+                addPointsToGraph();
+            }
+        });
 	}
-
-
+    
+    
 	/**
 	 *  Returns the panel attribute of the ShakeAnalysis object
 	 *
@@ -232,8 +233,8 @@ public class ShakeAnalysis {
 	public JPanel getPanel() {
 		return shakeAnalysisMainPanel;
 	}
-
-
+    
+    
 	/**
 	 *  Constructor for the setPVLoggerId object
 	 *
@@ -242,7 +243,7 @@ public class ShakeAnalysis {
 	public void setPVLoggerId(long pvLoggerId) {
 		pvLoggerIdTextField.setValue((double) pvLoggerId);
 	}
-
+    
 	/**
 	 *  Sets the tableModels attribute of the ShakeAnalysis object
 	 *
@@ -252,12 +253,12 @@ public class ShakeAnalysis {
 	public void setTableModels(QuadsTable quadsTableModel, BPMsTable bpmsTableModel) {
 		this.quadsTableModel = quadsTableModel;
 		this.bpmsTableModel = bpmsTableModel;
-
+        
 		quadsTable.setModel(quadsTableModel);
 		bpmsTable.setModel(bpmsTableModel);
 	}
-
-
+    
+    
 	/**
 	 *  Sets the orbitCorrector attribute of the ShakeAnalysis object
 	 *
@@ -266,8 +267,8 @@ public class ShakeAnalysis {
 	public void setOrbitCorrector(OrbitCorrector orbitCorrector) {
 		this.orbitCorrector = orbitCorrector;
 	}
-
-
+    
+    
 	/**
 	 *  Clear the graph region of the sub-panel.
 	 */
@@ -278,15 +279,15 @@ public class ShakeAnalysis {
 		messageTextLocal.setText(null);
 		messageTextLocal.setForeground(Color.red);
 	}
-
-
+    
+    
 	/**
 	 *  Description of the Method
 	 */
 	public void update() {
 	}
-
-
+    
+    
 	/**
 	 *  Sets the accelSeq attribute of the ShakeAnalysis object
 	 *
@@ -296,7 +297,7 @@ public class ShakeAnalysis {
 		this.accSeq = accSeq;
 		bpmIdMap.clear();
 		quadIdMap.clear();
-
+        
 		java.util.List<AcceleratorNode> quadList = accSeq.getAllNodesWithQualifier((new OrTypeQualifier()).or(Quadrupole.s_strType));
 		Iterator<AcceleratorNode> quad_itr = quadList.iterator();
 		while(quad_itr.hasNext()) {
@@ -306,7 +307,7 @@ public class ShakeAnalysis {
 				quadIdMap.put(quad.getId(), quad);
 			}
 		}
-
+        
 		java.util.List<AcceleratorNode> bpmList = accSeq.getAllNodesOfType("BPM");
 		Iterator<AcceleratorNode> bpm_itr = bpmList.iterator();
 		while(bpm_itr.hasNext()) {
@@ -314,12 +315,12 @@ public class ShakeAnalysis {
 			bpmIdMap.put(bpm.getId(), bpm);
 		}
 	}
-
+    
 	/**
 	 *  Description of the Method
 	 */
 	private void performAnalysis() {
-
+        
 		Scenario scenario = null;
 		try {
 			scenario = Scenario.newScenarioFor(accSeq);
@@ -327,10 +328,10 @@ public class ShakeAnalysis {
 			messageTextLocal.setText("Can not create scenario for this sequence! Stop!");
 			return;
 		}
-
+        
 		//custom part - depends on "Use Logger" button state
 		if(!usePVLoggerButton.isSelected()) {
-		        //scenario.setSynchronizationMode(Scenario.SYNC_MODE_DESIGN);
+            //scenario.setSynchronizationMode(Scenario.SYNC_MODE_DESIGN);
 			//scenario.setSynchronizationMode(Scenario.SYNC_MODE_LIVE);
 			scenario.setSynchronizationMode(Scenario.SYNC_MODE_RF_DESIGN);
 		} else {
@@ -339,30 +340,40 @@ public class ShakeAnalysis {
 			scenario = plds.setModelSource(accSeq, scenario);
 			orbitCorrector.setPVLoggerID(pvLogID);
 		}
-
-		xal.model.alg.EnvelopeTracker tracker = new xal.model.alg.EnvelopeTracker();
-		EnvelopeProbe probe = ProbeFactory.getEnvelopeProbe(accSeq, tracker);
-
+        
+        EnvelopeTracker tracker = null;
+        
+        try {
+            
+            tracker = AlgorithmFactory.createEnvelopeTracker( accSeq);
+            
+        } catch ( InstantiationException exception ) {
+            System.err.println( "Instantiation exception creating tracker." );
+            exception.printStackTrace();
+        }
+        
+        EnvelopeProbe probe = ProbeFactory.getEnvelopeProbe(accSeq, tracker);
+        
 		scenario.setProbe(probe);
 		scenario.resetProbe();
-    //probe.setBeamCharge(0.0);
-
+        //probe.setBeamCharge(0.0);
+        
 		try {
 			scenario.resync();
 		} catch(SynchronizationException e) {
 			messageTextLocal.setText("Can not synchronize scenario! Acc. is dead? Stop!");
 			return;
 		}
-
+        
 		try {
 			scenario.run();
 		} catch(ModelException e) {
 			messageTextLocal.setText("Can not run scenario! Stop!");
 			return;
 		}
-
+        
 		EnvelopeTrajectory trajectory = (EnvelopeTrajectory) probe.getTrajectory();
-
+        
 		for(int i = 0, n = quadsTableModel.getListModel().size(); i < n; i++) {
 			Quad_Element quadElm =  quadsTableModel.getListModel().get(i);
 			Quadrupole quad =  quadIdMap.get(quadElm.getName());
@@ -428,7 +439,7 @@ public class ShakeAnalysis {
 				}
 			}
 		}
-
+        
 		//The initialization of the correctors coefficients
 		//coeff = Q*L/(m*gamma*velocity) = c*L/(m[in eV]*gamma*beta)
 		Vector<Corr_Element> corrV = new Vector<Corr_Element>();
@@ -446,7 +457,7 @@ public class ShakeAnalysis {
 			double c = 2.997924E+8;
 			double res_coeff = (L * c) / (W0 * beta * gamma);
 			double corr_pos = accSeq.getPosition(corr_mag);
-
+            
 			for(int j = 0, m = quadsTableModel.getListModel().size(); j < m; j++) {
 				Quad_Element quadElm = quadsTableModel.getListModel().get(j);
 				Quadrupole quad =  quadIdMap.get(quadElm.getName());
@@ -469,8 +480,8 @@ public class ShakeAnalysis {
 			}
 		}
 	}
-
-
+    
+    
 	/**
 	 *  Adds a feature to the PointsToGraph attribute of the ShakeAnalysis object
 	 */
@@ -541,28 +552,28 @@ public class ShakeAnalysis {
 		graphXPanel.refreshGraphJPanel();
 		graphYPanel.refreshGraphJPanel();
 	}
-
-
+    
+    
 	/**
 	 *  Sets the fontForAll attribute of the ShakeAnalysis object
 	 *
 	 *@param  fnt  The new fontForAll value
 	 */
 	public void setFontForAll(Font fnt) {
-
+        
 		quadTableBorder.setTitleFont(fnt);
 		bpmTableBorder.setTitleFont(fnt);
 		analysisControlBorder.setTitleFont(fnt);
-
+        
 		quadsTable.setFont(fnt);
 		bpmsTable.setFont(fnt);
-
+        
 		int font_width = quadsTable.getFontMetrics(fnt).charWidth('U');
 		int font_height = quadsTable.getFontMetrics(fnt).getHeight();
-
+        
 		quadsTable.setRowHeight((int) 1.1 * font_height);
 		bpmsTable.setRowHeight((int) 1.1 * font_height);
-
+        
 		quadsTable.getColumnModel().getColumn(0).setPreferredWidth(30 * font_width);
 		quadsTable.getColumnModel().getColumn(0).setMaxWidth(2000);
 		quadsTable.getColumnModel().getColumn(0).setMinWidth(20 * font_width);
@@ -570,21 +581,21 @@ public class ShakeAnalysis {
 		quadsTable.getColumnModel().getColumn(1).setMinWidth(6 * font_width);
 		quadsTable.getColumnModel().getColumn(2).setMaxWidth(6 * font_width);
 		quadsTable.getColumnModel().getColumn(2).setMinWidth(6 * font_width);
-
+        
 		bpmsTable.getColumnModel().getColumn(0).setPreferredWidth(30 * font_width);
 		bpmsTable.getColumnModel().getColumn(0).setMaxWidth(2000);
 		bpmsTable.getColumnModel().getColumn(0).setMinWidth(20 * font_width);
 		bpmsTable.getColumnModel().getColumn(1).setMaxWidth(6 * font_width);
 		bpmsTable.getColumnModel().getColumn(1).setMinWidth(6 * font_width);
-
+        
 		usePVLoggerButton.setFont(fnt);
 		pvLoggerIdTextField.setFont(fnt);
 		analysisButton.setFont(fnt);
 		rePlotButton.setFont(fnt);
-
+        
 		validationCntr.setFontForAll(fnt);
 	}
-
+    
 	/**
 	 *  Adds a feature to the GraphData attribute of the ShakeAnalysis object
 	 *
@@ -593,29 +604,29 @@ public class ShakeAnalysis {
 	private void addGraphData(BPM_Element bpm) {
 		if(!graphXDataMap.containsKey(bpm)) {
 			Color color = IncrementalColors.getColor(graphXDataMap.size());
-
+            
 			BasicGraphData graphXData = new BasicGraphData();
 			graphXData.setDrawLinesOn(false);
 			graphXData.setGraphColor(color);
 			graphXData.setGraphPointSize(5);
 			graphXData.setImmediateContainerUpdate(false);
 			graphXData.setGraphProperty(graphXPanel.getLegendKeyString(), bpm.getName());
-
+            
 			graphXDataMap.put(bpm, graphXData);
 			graphXPanel.addGraphData(graphXData);
-
+            
 			BasicGraphData graphYData = new BasicGraphData();
 			graphYData.setDrawLinesOn(false);
 			graphYData.setGraphColor(color);
 			graphYData.setGraphPointSize(5);
 			graphYData.setImmediateContainerUpdate(false);
 			graphYData.setGraphProperty(graphYPanel.getLegendKeyString(), bpm.getName());
-
+            
 			graphYDataMap.put(bpm, graphYData);
 			graphYPanel.addGraphData(graphYData);
 		}
 	}
-
+    
 	/**
 	 *  Description of the Method
 	 */
@@ -625,18 +636,18 @@ public class ShakeAnalysis {
 			BasicGraphData grd =  x_itr.next();
 			grd.removeAllPoints();
 		}
-
+        
 		Iterator<BasicGraphData> y_itr = graphYDataMap.values().iterator();
 		while(y_itr.hasNext()) {
 			BasicGraphData grd =y_itr.next();
 			grd.removeAllPoints();
 		}
-
+        
 		graphPosXData.removeAllPoints();
 		graphPosYData.removeAllPoints();
 	}
-
-
+    
+    
 	/**
 	 *  Adds a feature to the GraphPointX attribute of the ShakeAnalysis object
 	 *
@@ -648,7 +659,7 @@ public class ShakeAnalysis {
 		BasicGraphData grdX = graphXDataMap.get(bpmElm);
 		grdX.addPoint(x, offSetX, 0.);
 	}
-
+    
 	/**
 	 *  Adds a feature to the GraphPointY attribute of the ShakeAnalysis object
 	 *
@@ -660,8 +671,8 @@ public class ShakeAnalysis {
 		BasicGraphData grdY =  graphYDataMap.get(bpmElm);
 		grdY.addPoint(x, offSetY, 0.);
 	}
-
-
+    
+    
 	/**
 	 *  Description of the Method
 	 */
@@ -675,7 +686,7 @@ public class ShakeAnalysis {
 		graphXPanel.addGraphData(graphPosXData);
 		graphYPanel.addGraphData(graphPosYData);
 	}
-
+    
 	/**
 	 *  Returns the messageText attribute of the ShakeAnalysis object
 	 *
@@ -684,6 +695,6 @@ public class ShakeAnalysis {
 	public JTextField getMessageText() {
 		return messageTextLocal;
 	}
-
+    
 }
 
