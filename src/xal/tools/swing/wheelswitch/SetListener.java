b6878a919af6d4d2568b30cb48a061c70bb08324
Dylan Maxwell <maxwelld@frib.msu.edu>
2013-09-16 13:31:13 -0400

    Add dependency 'SetListener' from Cosy library.

---
+++ src/xal/tools/swing/wheelswitch/SetListener.java
@@ -0,0 +1,23 @@
+/*
+ * @@COPYRIGHT@@
+ */
+package com.cosylab.events;
+
+import java.util.EventListener;
+
+/**
+ * This interface is used to notify the listeners of value change.
+ * 
+ * @author Ales Pucelj
+ * @version @@VERSION@@
+ */
+public interface SetListener extends EventListener {
+
+	/**
+	 * Notifies that a set command has occured.
+	 * 
+	 * @param e SetEvent
+	 */
+	void setPerformed(SetEvent e);
+
+}
