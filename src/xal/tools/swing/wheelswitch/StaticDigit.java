b848ae176009b82bb47e51dc1926e00571ccdf6b
Dylan Maxwell <maxwelld@frib.msu.edu>
2013-09-16 14:01:28 -0400

    Add dependency 'StaticDigit' from Cosy library.

---
+++ src/xal/tools/swing/wheelswitch/StaticDigit.java
@@ -0,0 +1,38 @@
+/*
+ * @@COPYRIGHT@@
+ */
+package com.cosylab.gui.components.wheelswitch;
+
+import com.cosylab.gui.components.SimpleButton;
+
+/**
+ * Descedant of <code>Digit</code> displaying static text.
+ * Objects of this class also cannot be selected. 
+ * 
+ * @author <a href="mailto:jernej.kamenik@cosylab.com">Jernej Kamenik</a>
+ * @version $id$
+ */
+public class StaticDigit extends Digit {
+
+	/**
+	 * Constructor for StaticDigit.
+	 * 
+	 * @param value to be displayed.
+	 * @see Digit#Digit()
+	 */			
+	public StaticDigit(String value) {
+		super();
+		setActionMode(SimpleButton.NULL_ACTION_MODE);
+		setText(value);
+	}
+
+	/**
+	 * This method has been overriden to disable selection of static digits.
+	 */
+	public void setSelected(boolean newSel){}
+
+	/**
+	 * This method has been overriden to disable selection of static digits.
+	 */
+	public boolean isSelected() {return false;}
+}
