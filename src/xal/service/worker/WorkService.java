78730af5f9403e6b074d77d4665c2da60a86f868
Thomas Pelaia II <pelaiata@ornl.gov>
2013-10-09 10:04:04 -0400

    -n Add 'openxal/' from commit '95fd95b30507ef8bc624a794c156eaddea16e253'
    git-subtree-dir: openxal
    git-subtree-mainline: 8883edbd916c964898271e50179acac8f71677ff
    git-subtree-split: 95fd95b30507ef8bc624a794c156eaddea16e253

---
+++ src/xal/service/worker/WorkService.java
@@ -0,0 +1,38 @@
+/*
+ * WorkService.java
+ *
+ * Created on Mon January 6 13:50:13 EST 2012
+ *
+ * Copyright (c) 2012 Spallation Neutron Source
+ * Oak Ridge National Laboratory
+ * Oak Ridge, TN 37830
+ */
+
+package xal.service.worker;
+
+import java.util.Date;
+
+
+/**
+ * Demo service providing demo work.
+ * @author  tap
+ */
+public class WorkService implements Working {
+    /** add two numbers */
+    public double add( final double summand, final double addend ) {
+        return summand + addend;
+    }
+
+    
+    /** get the launch time */
+    public Date getLaunchTime() {
+        return Main.getLaunchTime();
+    }
+    
+    
+    /** shutdown the service */
+    public void shutdown( final int code ) {
+        Main.shutdown( code );
+    }
+}
+
