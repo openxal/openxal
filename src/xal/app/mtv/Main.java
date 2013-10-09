b486750b5a358bb7ee5db1b0dadf7511af7e77d7
Thomas Pelaia II <pelaiata@ornl.gov>
2013-10-09 10:04:04 -0400

    -n Add 'openxal/' from commit '95fd95b30507ef8bc624a794c156eaddea16e253'
    git-subtree-dir: openxal
    git-subtree-mainline: 8883edbd916c964898271e50179acac8f71677ff
    git-subtree-split: 95fd95b30507ef8bc624a794c156eaddea16e253

---
+++ src/xal/app/mtv/Main.java
@@ -0,0 +1,107 @@
+package xal.app.mtv;
+
+import javax.swing.*;
+import javax.swing.event.*;
+import java.awt.event.*;
+import java.net.*;
+import java.io.*;
+
+import xal.application.*;
+import xal.smf.application.*;
+
+/**
+ * The main program for the xio  app.
+ * @author J. Galambos
+ *
+ */
+public class Main extends ApplicationAdaptor {
+
+    //-------------Constructors-------------
+    public Main() {url = null;}
+
+    public Main(String str) {
+
+	    try{
+		url = new URL(str);
+
+	    }
+	    catch (MalformedURLException exception) {
+		System.err.println(exception);
+	    }
+    }
+
+    // --------- Document management -------------------------------------------
+    
+    public String[] readableDocumentTypes() {
+        return new String[] {"mtv"};
+    }
+    
+    
+    public String[] writableDocumentTypes() {
+        return new String[] {"mtv"};
+    }
+ 
+    /** this returns a new document to start working with  */ 
+    public XalDocument newEmptyDocument() {
+      MTVDocument md;
+
+	// even if url == null, use it. A default accelerator will be created.
+
+	return md = new MTVDocument(url);
+    }
+        
+    public XalDocument newDocument(java.net.URL theUrl) {
+	MTVDocument md = new MTVDocument(theUrl);
+	return md;
+    }
+    
+    // --------- Global application management ---------------------------------
+    
+    
+    public String applicationName() {
+        return "mtv";
+    }
+
+
+    public boolean usesConsole() {
+        return false;
+    }
+    
+    //  -------- specific to mpspostmort application ---------
+
+    /** The url to use to create the startup document with */
+
+    private URL url;
+    
+    /**
+     * Register actions for the Special menu items
+     */
+    protected void customizeCommands(Commander commander) {
+    }
+    
+    
+    // --------- Application events --------------------------------------------
+    
+    public void applicationFinishedLaunching() { 
+	System.out.println("mtv finished launching...");
+    }
+    
+     /** The main method of the application. */
+    static public void main(String[] args) {
+        try {
+		URL[] urls = new URL[args.length];
+		for ( int index = 0 ; index < args.length ; index++ ) {
+			urls[index] = new File(args[index]).toURI().toURL();
+		}
+		AcceleratorApplication.launch(new Main(), urls );
+        }
+        catch(Exception exception) {
+            System.err.println( exception.getMessage() );
+            exception.printStackTrace();
+	    Application.displayApplicationError("Launch Exception", "Launch Exception", exception);
+	    System.exit(-1);
+        }
+    }
+
+
+}
