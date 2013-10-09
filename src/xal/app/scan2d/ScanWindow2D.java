33ddeee2c3bc3d853ad43dcd3b24c9449d1cf357
Thomas Pelaia II <pelaiata@ornl.gov>
2013-10-09 10:04:04 -0400

    -n Add 'openxal/' from commit '95fd95b30507ef8bc624a794c156eaddea16e253'
    git-subtree-dir: openxal
    git-subtree-mainline: 8883edbd916c964898271e50179acac8f71677ff
    git-subtree-split: 95fd95b30507ef8bc624a794c156eaddea16e253

---
+++ src/xal/app/scan2d/ScanWindow2D.java
@@ -0,0 +1,105 @@
+/*
+ * ScanWindow2D.java
+ *
+ * Created on July 31, 2003, 10:25 AM
+ */
+
+package xal.app.scan2d;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+
+import xal.application.*;
+import xal.smf.application.*;
+
+/**
+ * ScanWindow2D is a subclass of XalWindow used in the 2D scan. It has mainPanel
+ * and the message text field for messages. 
+ *
+ * @author  shishlo
+ */
+
+public class ScanWindow2D extends AcceleratorWindow {
+	private static final long serialVersionUID = 0L;
+	
+    private JPanel mainPanel = new JPanel(new BorderLayout());
+    private JTextField messageTextField = new JTextField();
+    
+    /** Creates a new instance of ScanWindow2D */
+    public ScanWindow2D(XalDocument aDocument) {
+        super(aDocument);
+        makeContent();
+        pack();
+        //setSize(500, 600);
+    }
+    
+    
+    /** 
+     * Getter of the text view that displays the document content.
+     * @return The main panel of the window.
+     */
+    JPanel getMainPanel() {
+        return mainPanel;
+    }
+
+    /** 
+     * Returns the currently displaying JComponent.
+     * @return The currently displaying JComponent.
+     */
+    JComponent getJComponent(){
+        return (JComponent) mainPanel.getComponent(0); 
+    }
+
+    /** 
+     * Sets the JComponent that will be visible in the main window.
+     */
+    void setJComponent(JComponent component){
+	mainPanel.removeAll();
+	if(component != null){
+	    mainPanel.add(component,BorderLayout.CENTER);
+	}
+	getContentPane().validate();
+        getContentPane().repaint();
+    }
+
+    /** 
+     * Returns the name of the currently displaying JComponent.
+     * @return The name of the currently displaying JComponent.
+     */
+    String getComponentName(){
+        return mainPanel.getComponent(0).getName(); 
+    }
+
+    /** 
+     * Returns the message text field.
+     * @return The message text field.
+     */
+    JTextField getMessageTextField(){
+        return messageTextField;
+    }    
+    
+    /**
+     * Create the main window subviews.
+     */
+    protected void makeContent() {
+        messageTextField.setForeground(Color.red);
+
+
+        JScrollPane scrollPane = new JScrollPane(mainPanel);
+
+	JPanel centerPanel = new JPanel(new BorderLayout());
+	centerPanel.add(scrollPane,BorderLayout.CENTER);
+	centerPanel.add(messageTextField,BorderLayout.SOUTH);
+
+        getContentPane().add(centerPanel,BorderLayout.CENTER);
+    } 
+
+    /**
+     * Dispose of ScanWindow2D resources.  
+     * This method overrides an empty superclass method.
+     */
+    protected void freeCustomResources(){
+	mainPanel = null;  
+    }  
+}
