aa308cfec4338b85ea93705f20989a62b0693357
Thomas Pelaia II <pelaiata@ornl.gov>
2013-10-16 15:52:44 -0400

    -n Add support for extensions directly under the extensions directory and move the scan code out of core and into an extension.

---
+++ src/xal/tools/scan/BeamTrigger.java
@@ -0,0 +1,132 @@
+package xal.tools.scan;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.event.*;
+import java.text.DecimalFormat;
+import java.awt.Font;
+import java.awt.Color;
+import java.awt.BorderLayout;
+
+import xal.ca.*;
+import xal.tools.swing.*;
+import xal.smf.TimingCenter;
+
+
+public class BeamTrigger{
+
+    //name of the beam trigger PV
+    private String triggerNamePV = "ICS_Tim:Gate_BeamOn:SSTrigger";
+    private Channel ch   = null;
+   
+    private JRadioButton useTriggerButton  = new JRadioButton(" Use Beam Trigger, Delay [sec]:");
+
+    private DoubleInputTextField tDelayText = new DoubleInputTextField(5);
+
+    private DecimalFormat tDelayFormat = new DecimalFormat("0.0#");
+
+    public BeamTrigger(){
+         
+	tDelayText.setHorizontalAlignment(JTextField.CENTER);
+        tDelayText.setNormalBackground(Color.white);
+        tDelayText.setDecimalFormat(tDelayFormat);
+	tDelayText.setValue(0.2);
+        
+	//get channel from timing center
+	final TimingCenter tmCenter = TimingCenter.getDefaultTimingCenter();
+		// some sites may not have a default accelerator defined or the trigger channel
+		if ( tmCenter != null ) {
+			ch = tmCenter.findChannel( TimingCenter.TRIGGER_HANDLE );
+			if ( ch != null ) {
+				useTriggerButton.setSelected(true);
+
+				useTriggerButton.addItemListener(new ItemListener(){
+					public void itemStateChanged(ItemEvent e) {
+						if (e.getStateChange() == ItemEvent.SELECTED) {
+							setOnOff(true);
+						}
+						else{
+							setOnOff(false);
+						}
+					}
+				});
+			}
+			else {
+				useTriggerButton.setSelected( false );
+				useTriggerButton.setEnabled( false );
+			}
+		}
+		else {
+			useTriggerButton.setSelected( false );
+			useTriggerButton.setEnabled( false );
+		}
+  
+    }
+
+    public void setOnOff(boolean onOff){
+	tDelayText.setEditable(onOff);
+        useTriggerButton.setSelected(onOff);
+    }
+
+    public void setFontForAll(Font fnt){
+	tDelayText.setFont(fnt);
+	useTriggerButton.setFont(fnt);
+    }
+
+    public JPanel getJPanel(){
+	JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.add(useTriggerButton,BorderLayout.CENTER);
+        panel.add(tDelayText,BorderLayout.EAST);
+	return panel;
+    }
+
+    public boolean isOn(){
+	return useTriggerButton.isSelected();
+    }
+
+    public void makePulse(){
+	if(useTriggerButton.isSelected() && ch != null){
+	    try {
+		ch.putVal(1.0);
+	    }
+	    catch (ConnectionException e){
+		setOnOff(false);
+		return;
+	    }
+	    catch (PutException e){ 
+		setOnOff(false);
+		return;
+	    }
+	    try{Thread.sleep((long) (tDelayText.getValue()*1000.0));}
+	    catch(InterruptedException e){}
+	}
+    }
+
+    public String getChannelName(){
+	return triggerNamePV ;
+    }
+
+    public Channel getChannel(){
+	return ch;
+    }
+
+    public void setChannelName(String chanName){
+	triggerNamePV = chanName;
+	ch = ChannelFactory.defaultFactory().getChannel(triggerNamePV);
+    }
+
+    public void setChannel(Channel ch_In){
+	ch = ch_In;
+        triggerNamePV = ch.channelName();
+    }
+
+
+    public void setDelay(double timeDelay){
+	tDelayText.setValue(timeDelay);
+    }
+
+    public double getDelay(){
+	return tDelayText.getValue();
+    }
+}
---
+++ src/xal/tools/scan/ScanVariable.java
@@ -0,0 +1,137 @@
+package xal.tools.scan;
+
+import javax.swing.*;
+import java.util.*;
+import java.awt.event.*;
+
+import xal.ca.*;
+
+public class ScanVariable{
+
+    private MonitoredPV mpv    = null;
+    private MonitoredPV mpvRB = null;
+
+    private double memValue      = 0.0;
+
+    //message text to report about problems
+    private JTextField messageText = null;
+
+    private ActionListener stopScanListener = null;
+    private ActionEvent stopActionEvent = null;
+
+    //synchronizing lock
+    private Object lockObj = new Object();
+
+    public ScanVariable(String alias, String aliasRB){
+	mpv   =MonitoredPV.getMonitoredPV(alias);
+ 	mpvRB =MonitoredPV.getMonitoredPV(aliasRB);
+	stopActionEvent = new ActionEvent(this,0,"stop");
+    }
+
+    protected void setStopScanListener(ActionListener stopScanListener){
+	this.stopScanListener = stopScanListener;
+    }
+
+    public void setMessageTextField(JTextField messageText){
+	this.messageText = messageText;
+    }
+
+    protected void setLockObject(Object lockObj){
+	this.lockObj = lockObj;
+    }
+
+    public String getChannelName(){
+	return mpv.getChannelName();
+    }
+
+    public String getChannelNameRB(){
+	return mpvRB.getChannelName();
+    }
+
+    public Channel getChannel(){
+	return mpv.getChannel();
+    }
+
+    public Channel getChannelRB(){
+	return mpvRB.getChannel();
+    }
+
+    public MonitoredPV getMonitoredPV(){
+	return mpv;
+    }
+
+    public MonitoredPV getMonitoredPV_RB(){
+	return mpvRB;
+    }
+
+    public void setChannelName(String chanName){
+	synchronized(lockObj){
+	    mpv.setChannelName(chanName);
+	}
+    }
+
+    public void setChannelNameRB(String chanNameRB){
+	synchronized(lockObj){
+	    mpvRB.setChannelName(chanNameRB);
+	}
+    }
+
+    public void setChannel(Channel ch){
+	synchronized(lockObj){
+	    mpv.setChannel(ch);
+	}
+    }
+
+    public void setChannelRB(Channel ch_RB){
+	synchronized(lockObj){
+	    mpvRB.setChannel(ch_RB);
+	}
+    }
+
+    public void memorizeValue(){
+	memValue = mpv.getValue();
+    }
+
+    public void restoreFromMemory(){
+	setValue(memValue);
+    }
+
+    public void setValue(double val){
+	Channel ch = mpv.getChannel();
+	String chanName = mpv.getChannelName();
+	if(ch != null){
+	    try {
+		ch.putVal(val);
+	    }
+	    catch (ConnectionException e){
+		stopScanWithMessage("Cannot put value to the channel: "+chanName);
+                return;
+	    }
+	    catch (PutException e){
+		stopScanWithMessage("Cannot put value to the channel: "+chanName);
+                return;
+	    }
+	}
+    }
+
+    public double getValue(){
+	return mpv.getValue();
+    }
+
+    public double getValueRB(){
+	return mpvRB.getValue();
+    }
+
+    private void stopScanWithMessage(String msg){
+	if(stopScanListener != null){
+	    stopScanListener.actionPerformed(stopActionEvent);
+	}
+	if(messageText != null){
+	    messageText.setText(msg);
+	}
+	else{
+	    System.out.println("ScanVariable class:"+msg);
+	}
+    }
+
+}
