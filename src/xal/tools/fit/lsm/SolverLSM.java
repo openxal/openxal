0d433a77d810f0d42ba1e4d6d64e573a379195a6
Thomas Pelaia II <pelaiata@ornl.gov>
2013-10-17 16:13:38 -0400

    -n -n Move bricks, the plot package and other widgets from the core to extensions.

--- src/xal/tools/fit/lsm/SolverLSM.java
+++ src/xal/tools/fit/lsm/SolverLSM.java
@@ -1,6 +1,6 @@
 package xal.tools.fit.lsm;
 
-import xal.tools.plot.*;
+import xal.tools.ArrayMath;
 
 /**
  *  The least square method solver
@@ -118,7 +118,7 @@ public class SolverLSM implements FitSolver {
 			}
 		}
 
-		boolean res = GraphDataOperations.reverseMatrix(ATWA);
+		boolean res = ArrayMath.invertMatrix(ATWA);
 		if (res != true) {
 			return false;
 		}
