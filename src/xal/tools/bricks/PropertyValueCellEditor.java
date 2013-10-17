89bf753b96f85d56a1db660bb97baa7a68b2e54b
Thomas Pelaia II <pelaiata@ornl.gov>
2013-10-17 16:13:38 -0400

    -n Move bricks, the plot package and other widgets from the core to extensions.

---
+++ src/xal/tools/bricks/PropertyValueCellEditor.java
@@ -0,0 +1,94 @@
+//
+//  PropertyValueCellEditor.java
+//  xal
+//
+//  Created by Thomas Pelaia on 7/6/06.
+//  Copyright 2006 Oak Ridge National Lab. All rights reserved.
+//
+
+package xal.tools.bricks;
+
+import java.beans.*;
+import java.lang.reflect.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import javax.swing.border.*;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.event.*;
+
+
+/** cell editor for property values */
+public class PropertyValueCellEditor extends AbstractCellEditor implements TableCellEditor, TableCellRenderer {
+    /** serialization ID */
+    private static final long serialVersionUID = 1L;
+    
+	/** context in which bricks is run */
+	final private BricksContext CONTEXT;
+	
+	final protected PropertyValueEditorManager PROPERTY_EDITOR_MANAGER;
+	final protected PropertyTableModel TABLE_MODEL;
+	protected PropertyValueEditor<?> _currentEditor;
+	
+	
+	/** Constructor */
+	public PropertyValueCellEditor( final BricksContext context, final JTable table, final PropertyValueEditorManager propertyEditorManager, final PropertyTableModel tableModel ) {
+		CONTEXT = context;
+		PROPERTY_EDITOR_MANAGER = propertyEditorManager;
+		TABLE_MODEL = tableModel;
+	}
+	
+	
+	/** get the editor for the property corresponding to the specified table row */
+	protected PropertyValueEditor<?> getEditor( final int row ) {
+		final Class<?> propertyClass = TABLE_MODEL.getPropertyClass( row );
+		PropertyValueEditor<?> editor;
+		
+		if ( propertyClass != null && PROPERTY_EDITOR_MANAGER.hasEditor( propertyClass ) ) {
+			editor = PROPERTY_EDITOR_MANAGER.getEditor( propertyClass );
+		}
+		else {
+			editor = PROPERTY_EDITOR_MANAGER.getEditor( String.class );
+		}
+		
+		return editor;
+	}
+	
+	
+	/** get the component */
+	public Component getTableCellEditorComponent( final JTable table, final Object value, final boolean isSelected, final int row, final int column ) {
+		_currentEditor = getEditor( row );
+		_currentEditor.setCurrentCellEditor( this );
+		_currentEditor.setEditorValue( value );
+		return _currentEditor.getEditorComponent();
+	}
+	
+	
+	/** get the cell editor value */
+	public Object getCellEditorValue() {
+		return _currentEditor.getEditorValue( CONTEXT );
+	}
+	
+	
+	/**
+	 * Get the table component.
+	 */
+	final public Component getTableCellRendererComponent( final JTable table, final Object value, final boolean isSelected, final boolean hasFocus, final int row, final int column ) {
+		final PropertyValueEditor<?> editor = getEditor( row );
+		final JComponent component = (JComponent)editor.getRenderingComponent();
+		editor.setRenderingValue( value );
+		component.setOpaque( isSelected );
+		return component;
+	}
+	
+	
+	/** Make this method public */
+	public void fireEditingStopped() {
+		super.fireEditingStopped();
+	}
+}
---
+++ src/xal/tools/bricks/PropertyValueTextEditor.java
@@ -0,0 +1,54 @@
+//
+//  PropertyValueTextFieldEditor.java
+//  xal
+//
+//  Created by Thomas Pelaia on 7/5/06.
+//  Copyright 2006 Oak Ridge National Lab. All rights reserved.
+//
+
+package xal.tools.bricks;
+
+import java.beans.*;
+import java.lang.reflect.*;
+import javax.swing.*;
+import javax.swing.table.*;
+import javax.swing.border.*;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.event.*;
+
+
+/** property value editor */
+abstract public class PropertyValueTextEditor<T> extends PropertyValueEditor<T> {
+	/** instantiate a component */
+	public Component getRenderingComponentInstance() {
+		return new JLabel();
+	}
+	
+	
+	/** instantiate a component */
+	public Component getEditorComponentInstance() {
+		return new JTextField();
+	}
+	
+	
+	/** get the cell editor value */
+	abstract public T getEditorValue( final BricksContext context );
+	
+	
+	/** set the editor value */
+	public void setEditorValue( final Object value ) {
+		((JTextField)getEditorComponent()).setText( value != null ? value.toString() : "" );
+	}
+	
+	
+	/** set the rendering value */
+	public void setRenderingValue( final Object value ) {
+		((JLabel)getRenderingComponent()).setText( value != null ? value.toString() : "" );
+	}
+}
