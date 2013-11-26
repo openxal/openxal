cac7689abaae81cf411ca6454cb0daab19f394f3
Thomas Pelaia II <pelaiata@ornl.gov>
2013-11-26 10:52:31 -0500

    -n Fix the Machine Simulator application to work with the new online model.

--- src/xal/app/machinesimulator/MachineSimulation.java
+++ src/xal/app/machinesimulator/MachineSimulation.java
@@ -15,37 +15,33 @@ import java.util.List;
 import xal.model.probe.Probe;
 import xal.model.probe.traj.ProbeState;
 import xal.model.probe.traj.Trajectory;
+import xal.tools.beam.calc.SimpleSimResultsAdaptor;
 
 
 /** MachineSimulation is the simulation result for a simulation on an accelerator sequence  */
 public class MachineSimulation {
     /** states for every element */
-//    final List<IPhaseState> ELEMENT_STATES;
-    final List<ProbeState> ELEMENT_STATES;
+    final List<MachineSimulationRecord> SIMULATION_RECORDS;;
     
     
 	/** Constructor */
     public MachineSimulation( final Probe probe ) {
         final Trajectory trajectory = probe.getTrajectory();
-        
-//        ELEMENT_STATES = new ArrayList<IPhaseState>( trajectory.numStates() );
-        ELEMENT_STATES = new ArrayList<ProbeState>( trajectory.numStates() );
+		final SimpleSimResultsAdaptor resultsAdaptor = new SimpleSimResultsAdaptor( trajectory );
+
+        SIMULATION_RECORDS = new ArrayList<MachineSimulationRecord>( trajectory.numStates() );
         
         final Iterator<ProbeState> stateIter = trajectory.stateIterator();
         while ( stateIter.hasNext() ) {
-//            final IPhaseState state = (IPhaseState)stateIter.next();
             final ProbeState state = stateIter.next();
-            ELEMENT_STATES.add( state );
-//            System.out.println( state.getElementId() );
-//            System.out.println( "   position: " + state.getPosition() );
-//            System.out.println( "" );
+			final MachineSimulationRecord simulationRecord = new MachineSimulationRecord( resultsAdaptor, state );
+            SIMULATION_RECORDS.add( simulationRecord );
         }
     }
     
     
-    /** Get the states */
-//    public List<IPhaseState> getStates() {
-    public List<ProbeState> getStates() {
-        return ELEMENT_STATES;
+    /** Get the simulation records */
+    public List<MachineSimulationRecord> getSimulationRecords() {
+        return SIMULATION_RECORDS;
     }
 }
