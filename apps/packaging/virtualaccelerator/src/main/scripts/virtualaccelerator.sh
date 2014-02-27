71cfc9a076b42258f8f43318200bdf281706e85c
Miroslav Pavleski <miroslav.pavleski@cosylab.com>
2014-02-28 00:26:42 +0100

    Fixed VA startup script

--- apps/packaging/virtualaccelerator/src/main/scripts/virtualaccelerator.sh
+++ apps/packaging/virtualaccelerator/src/main/scripts/virtualaccelerator.sh
@@ -3,4 +3,4 @@ CURRENT_DIR=`dirname $0`
 LINKTARGET=`readlink -f $CURRENT_DIR/virtualaccelerator`
 DIR=`dirname $LINKTARGET`
 cd $DIR/../lib/openxal && 
-java -cp "openxal.apps.virtualaccelerator-1.0.0-SNAPSHOT.jar:*" -DOPENXAL_CONF=${CODAC_CONF}/openxal xal.app.virtualaccelerator.Main
+java -cp "openxal.apps.virtualaccelerator-1.0.0-SNAPSHOT.jar:*" -Dcom.cosylab.epics.caj.CAJContext.addr_list=127.0.0.1 -Dcom.cosylab.epics.caj.CAJContext.auto_addr_list=false -DOPENXAL_CONF=${CODAC_CONF}/openxal xal.app.virtualaccelerator.Main
