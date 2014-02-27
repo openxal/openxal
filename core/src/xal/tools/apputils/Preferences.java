5dece31e4f0c409b0384874fd54309aced4fcccf
Ivo List <ivo.list@cosylab.com>
2014-02-27 20:35:28 +0100

    implemented general preferences with reading from etc

--- core/src/xal/tools/apputils/Preferences.java
+++ core/src/xal/tools/apputils/Preferences.java
@@ -1,224 +1,120 @@
 package xal.tools.apputils;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.Properties;
 import java.util.Set;
+import java.util.prefs.AbstractPreferences;
 import java.util.prefs.BackingStoreException;
-import java.util.prefs.NodeChangeListener;
-import java.util.prefs.PreferenceChangeListener;
 
-public class Preferences extends java.util.prefs.Preferences {
-	protected java.util.prefs.Preferences usr, sys;
-	protected Set<String> usrKeys, sysKeys;
-
-	protected Preferences(java.util.prefs.Preferences usr, java.util.prefs.Preferences sys)
-	{
-		this.usr = usr;
-		this.sys = sys;
-	}	
+public class Preferences extends java.util.prefs.AbstractPreferences {
+	protected java.util.prefs.Preferences userPrefs;
+	protected Properties sysPrefs;
+	protected Set<String> usrKeys;
 	
-	protected boolean useUser(String key)
+	protected Preferences(java.util.prefs.Preferences userPrefs, String name) {
+		super(parentPrefs(userPrefs), name);
+		this.userPrefs = userPrefs;
+	}
+
+	private static Preferences parentPrefs(java.util.prefs.Preferences userPrefs) {
+		java.util.prefs.Preferences parentPrefs = userPrefs.parent();
+		if (parentPrefs == null) return null;
+		return new Preferences(parentPrefs, parentPrefs.name());
+	}
+
+	@Override
+	protected AbstractPreferences childSpi(String name) {
+		java.util.prefs.Preferences child = userPrefs.node(name);
+		return new Preferences(child, child.name());
+	}
+
+	@Override
+	protected String[] childrenNamesSpi() throws BackingStoreException {
+		return userPrefs.childrenNames();
+	}
+
+	@Override
+	protected void flushSpi() throws BackingStoreException {
+		userPrefs.flush();
+	}
+	
+	private boolean usrContains(String key) {
+		String keys[];
+		try {
+			keys = userPrefs.keys();
+			for (String userKey : keys)
+				if (key.equals(userKey)) return true;
+		} catch (BackingStoreException e) {
+		}
+		return false;
+	}
+
+	protected String fullName() {
+		if ("".equals(name())) return "xal";
+		Preferences parent = (Preferences)parent();
+		return parent == null || "".equals(parent.name()) ? name() : parent.fullName() + "." + name();
+	}
+	
+	protected Properties getSysPrefs()
 	{
-		if (usrKeys == null) {
-			try {
-				sysKeys = new HashSet<String>(Arrays.asList(sys.keys()));			
-			} catch (BackingStoreException e) {
-				sysKeys = new HashSet<>();
-			}
-			try {
-				usrKeys = new HashSet<String>(Arrays.asList(usr.keys()));			
-			} catch (BackingStoreException e) {
-				usrKeys = new HashSet<>();
+		if (sysPrefs == null) {
+			File confFile = new File(System.getProperty("OPENXAL_CONF", "/etc/openxal") + "/" + fullName());
+			System.out.printf("%s\n", confFile.toString());
+			if (confFile.exists()) {
+				sysPrefs = new Properties();
+				try {
+					sysPrefs.load(new FileReader(confFile));
+				} catch (IOException e) {
+				}
 			}
 		}
-		return usrKeys.contains(key) || !sysKeys.contains(key);
+		return sysPrefs;
 	}
 	
 	@Override
-	public String absolutePath() {
-		return usr.absolutePath();
+	protected String getSpi(String key) {
+		if (usrContains(key) || !getSysPrefs().contains(key)) return userPrefs.get(key, null);
+		return getSysPrefs().getProperty(key);
 	}
 
 	@Override
-	public void addNodeChangeListener(NodeChangeListener ncl) {
-		usr.addNodeChangeListener(ncl);
-		
+	protected String[] keysSpi() throws BackingStoreException {
+		Set<String> keys = getSysPrefs().stringPropertyNames();
+		keys.addAll(Arrays.asList(userPrefs.keys()));
+		return keys.toArray(new String[keys.size()]);
 	}
 
 	@Override
-	public void addPreferenceChangeListener(PreferenceChangeListener pcl) {
-		usr.addPreferenceChangeListener(pcl);
-		
+	protected void putSpi(String key, String value) {
+		userPrefs.put(key, value);
 	}
 
 	@Override
-	public String[] childrenNames() throws BackingStoreException {
-		return usr.childrenNames();
+	protected void removeNodeSpi() throws BackingStoreException {
+		userPrefs.removeNode();
 	}
 
 	@Override
-	public void clear() throws BackingStoreException {
-		usr.clear();	
-		usrKeys = null;
+	protected void removeSpi(String key) {
+		userPrefs.remove(key);
 	}
 
 	@Override
-	public void exportNode(OutputStream os) throws IOException,
-			BackingStoreException {
-		usr.exportNode(os);
-		
-	}
-
-	@Override
-	public void exportSubtree(OutputStream os) throws IOException,
-			BackingStoreException {
-		usr.exportSubtree(os);
-		
-	}
-
-	@Override
-	public void flush() throws BackingStoreException {
-		usr.flush();		
-	}
-
-	@Override
-	public String get(String key, String def) {
-		return useUser(key) ? usr.get(key, def) : sys.get(key, def);
-	}
-
-	@Override
-	public boolean getBoolean(String key, boolean def) {
-		return useUser(key) ? usr.getBoolean(key, def) : sys.getBoolean(key, def);
-	}
-
-	@Override
-	public byte[] getByteArray(String key, byte[] def) {
-		return useUser(key) ? usr.getByteArray(key, def) : sys.getByteArray(key, def);
-	}
-
-	@Override
-	public double getDouble(String key, double def) {
-		return useUser(key) ? usr.getDouble(key, def) : sys.getDouble(key, def);
-	}
-
-	@Override
-	public float getFloat(String key, float def) {
-		return useUser(key) ? usr.getFloat(key, def) : sys.getFloat(key, def);
-	}
-
-	@Override
-	public int getInt(String key, int def) {
-		return useUser(key) ? usr.getInt(key, def) : sys.getInt(key, def);
-	}
-
-	@Override
-	public long getLong(String key, long def) {
-		return useUser(key) ? usr.getLong(key, def) : sys.getLong(key, def);
-	}
-
-	@Override
-	public boolean isUserNode() {
-		return true;
-	}
-
-	@Override
-	public String[] keys() throws BackingStoreException {
-		return usr.keys();
-	}
-
-	@Override
-	public String name() {
-		return usr.name();
-	}
-
-	@Override
-	public java.util.prefs.Preferences node(String pathName) {
-		return new Preferences(usr.node(pathName), sys.node(pathName));
-	}
-
-	@Override
-	public boolean nodeExists(String pathName) throws BackingStoreException {
-		return usr.nodeExists(pathName) || sys.nodeExists(pathName);
-	}
-
-	@Override
-	public java.util.prefs.Preferences parent() {
-		return new Preferences(usr.parent(), sys.parent());
-	}
-
-	@Override
-	public void put(String key, String value) {
-		usr.put(key, value);		
-	}
-
-	@Override
-	public void putBoolean(String key, boolean value) {
-		usr.putBoolean(key, value);		
-	}
-
-	@Override
-	public void putByteArray(String key, byte[] value) {
-		usr.putByteArray(key, value);
-	}
-
-	@Override
-	public void putDouble(String key, double value) {
-		usr.putDouble(key, value);		
-	}
-
-	@Override
-	public void putFloat(String key, float value) {
-		usr.putFloat(key, value);
-	}
-
-	@Override
-	public void putInt(String key, int value) {
-		usr.putInt(key, value);
-	}
-
-	@Override
-	public void putLong(String key, long value) {
-		usr.putLong(key, value);
-	}
-
-	@Override
-	public void remove(String key) {
-		usr.remove(key);	
-		usrKeys = null;
-	}
-
-	@Override
-	public void removeNode() throws BackingStoreException {
-		usr.removeNode();		
-		usrKeys = null;
-	}
-
-	@Override
-	public void removeNodeChangeListener(NodeChangeListener ncl) {
-		usr.removeNodeChangeListener(ncl);
-	}
-
-	@Override
-	public void removePreferenceChangeListener(PreferenceChangeListener pcl) {
-		usr.removePreferenceChangeListener(pcl);		
-	}
-
-	@Override
-	public void sync() throws BackingStoreException {
-		usr.sync();		
-		usrKeys = null;
-	}
-
-	@Override
-	public String toString() {
-		return usr.toString();
+	protected void syncSpi() throws BackingStoreException {
+		userPrefs.sync();
 	}
 	
 	public static java.util.prefs.Preferences nodeForPackage(Class<?> c)
 	{
-		return new Preferences(Preferences.userNodeForPackage(c),
-				Preferences.systemNodeForPackage(c));
+		java.util.prefs.Preferences userPrefs = Preferences.userNodeForPackage(c);
+		return new Preferences(userPrefs, userPrefs.name());
 	}
+	
+	/*public static void main(String[] args) {
+		java.util.prefs.Preferences prefs = nodeForPackage(xal.sim.scenario.Scenario.class);
+		System.out.printf("%s\n", prefs.get("configURL", "blah"));
+	}*/
 }
