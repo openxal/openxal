223bdec7aeaac702f8fc21c0aa8a3e3e19982bad
Miroslav Pavleski <miroslav.pavleski@cosylab.com>
2014-04-15 13:51:53 +0200

    OXAL-313 Tagged OpenXAL 1.0.0

--- apps/packaging/machinesimulator/src/main/scripts/machinesimulator.sh
+++ apps/packaging/machinesimulator/src/main/scripts/machinesimulator.sh
@@ -3,4 +3,4 @@ CURRENT_DIR=`dirname $0`
 LINKTARGET=`readlink -f $CURRENT_DIR/machinesimulator`
 DIR=`dirname $LINKTARGET`
 cd $DIR/../lib/openxal && 
-java -jar "openxal.apps.machinesimulator-1.0.0-SNAPSHOT.jar"
+java -jar "openxal.apps.machinesimulator-1.0.0.jar"
