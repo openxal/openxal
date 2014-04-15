04bd940e379a5200caf9dc76e0a4f3920365bd7d
Miroslav Pavleski <miroslav.pavleski@cosylab.com>
2014-04-15 11:37:32 +0200

    OXAL-313 Preparing for plain Java Maven Version bump to 1.0.0

--- services/pvlogger/packaging/src/main/scripts/pvlogger-service.sh
+++ services/pvlogger/packaging/src/main/scripts/pvlogger-service.sh
@@ -3,4 +3,4 @@ CURRENT_DIR=`dirname $0`
 LINKTARGET=`readlink -f $CURRENT_DIR/pvlogger-service`
 DIR=`dirname $LINKTARGET`
 cd $DIR/../lib/openxal && 
-java -cp "openxal.service.pvlogger-1.0.0-SNAPSHOT.jar:*" -DOPENXAL_CONF=${CODAC_CONF}/openxal xal.service.pvlogger.Main
+java -cp "openxal.service.pvlogger-1.0.0.jar:*" -DOPENXAL_CONF=${CODAC_CONF}/openxal xal.service.pvlogger.Main
