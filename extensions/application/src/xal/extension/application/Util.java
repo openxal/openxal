f44b8189cd2f0f7167a1eb28bc06d27a7c4467e4
Thomas Pelaia II <pelaiata@ornl.gov>
2014-05-15 15:17:56 -0400

    merge in changes to the application framework in support of script based applications

--- extensions/application/src/xal/extension/application/Util.java
+++ extensions/application/src/xal/extension/application/Util.java
@@ -6,7 +6,10 @@
 
 package xal.extension.application;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.*;
+
 
 /**
  * Utility class providing convenience methods for use in the application framework.
@@ -21,40 +24,55 @@ public class Util {
     
     
     /**
-     * Load the resource bundle specified by the path.  The path should be 
-     * given in Java classpath notation and should exclude the "properties" 
-     * suffix of the properties file.  When the resource bundle is loaded, 
-     * the properties are stored in a Map for convenience data access.
-     * @param path The path to the properties file
-     * @return The map equivalent of the resource bundle
-     * @throws java.util.MissingResourceException If the resource bundle cannot be found.
+     * Load the resource bundle specified by the URL.  When the resource bundle is loaded, the properties are stored in a Map for convenience data access.
+     * @param resourceURL The URL to the properties file
+     * @return The map equivalent of the resource bundle or null if the resource was not found
      */
-    static public Map<String,String> loadResourceBundle( final String path ) throws MissingResourceException {
-        final Map<String,String> infoMap = new HashMap<String,String>();
-        final ResourceBundle bundle = ResourceBundle.getBundle( path );
-        
-        final Enumeration<String> keyEnum = bundle.getKeys();
-        while( keyEnum.hasMoreElements() ) {
-            final String key = keyEnum.nextElement();
-            infoMap.put( key, bundle.getString( key ) );
-        }
-        
-        return infoMap;
+    static public Map<String,String> loadResourceBundle( final URL resourceURL ) throws RuntimeException {
+		final Map<String,String> infoMap = new HashMap<>();
+
+		if ( resourceURL == null )  return infoMap;
+
+		try {
+			final InputStream inputStream = resourceURL.openStream();
+			final Properties properties = new Properties();
+			properties.load( inputStream );
+
+			for ( final Map.Entry<Object,Object> entry : properties.entrySet() ) {
+				final Object key = entry.getKey();
+				final String keyString = key != null ? key.toString() : null;
+				final Object value = entry.getValue();
+				final String valueString = entry != null ? value.toString() : null;
+
+				infoMap.put( keyString, valueString );
+			}
+
+			return infoMap;
+		}
+		catch( java.io.FileNotFoundException exception ) {
+			// this may be fine as the resource may be optional
+			return null;	// return null to indicate that the resource was missing
+		}
+		catch( Exception exception ) {
+			throw new RuntimeException( "Exception loading bundle from resource: " + resourceURL, exception );
+		}
     }
 	
 	
 	/**
-	 * Merge the resource bundle at the path into the specified map.
+	 * Merge the resource bundle from the specified source into the specified map. If the file does not exist at the source, then nothing is merged as this is intened for optional modifications.
 	 * @param map the map into which the resources should be merged
-	 * @param path the path to the resource bundle
+	 * @param source URL to the resource to merge
 	 */
-    static public void mergeResourceBundle( final Map<String,String> map, final String path ) throws MissingResourceException {
-        final ResourceBundle bundle = ResourceBundle.getBundle( path );
-		
-        final Enumeration<String> keyEnum = bundle.getKeys();
-        while( keyEnum.hasMoreElements() ) {
-            final String key = keyEnum.nextElement();
-			final String assignment = bundle.getString( key );
+    static public void mergeResourceBundle( final Map<String,String> map, final URL source ) {
+		final Map<String,String> sourceBundle = loadResourceBundle( source );
+
+		// if the bundle at the source was not found then we have nothing to merge so we are done
+		if ( sourceBundle == null )  return;
+
+		// merge the properties from the source onto the map (overriding it if conflicting)
+		for ( final String key : sourceBundle.keySet() ) {
+			final String assignment = sourceBundle.get( key );
 			
 			// if the key begins with "+" then prepend the new assignment onto the existing assignment
 			if ( key.startsWith( "+" ) ) {
@@ -87,15 +105,14 @@ public class Util {
 	
 	
 	/**
-	 * Convenience method for loading the resource bundle from a file whose path is specified relative
-	 * to the application's resources folder.
-	 * @param propertyFile The property file path relative to the application's resources folder
+	 * Convenience method for loading the resource bundle from a file whose path is specified relative to the application's resources folder.
+	 * @param propertyFile The property file relative to the application's resources folder
 	 * @return The map equivalent of the resource bundle
      * @throws java.util.MissingResourceException If the resource bundle cannot be found.
 	 * @see #loadResourceBundle
 	 */
-	static public Map<String,String> getPropertiesFromResource(String propertyFile) throws MissingResourceException {
-		return loadResourceBundle( Application.getAdaptor().getResourcesPath() + "." + propertyFile );
+	static public Map<String,String> getPropertiesForResource( final String propertyFile ) {
+		return loadResourceBundle( Application.getAdaptor().getResourceURL( propertyFile ) );
 	}
     
     
