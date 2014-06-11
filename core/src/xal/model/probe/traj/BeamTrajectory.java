ecde0297476bbecc9fc62fd86b64c129243ac723
freedjm <freedjm@ornl.gov>
2014-06-11 10:03:34 -0400

    Commented out all Trajectory subclasses to simulate removing them.

--- core/src/xal/model/probe/traj/BeamTrajectory.java
+++ core/src/xal/model/probe/traj/BeamTrajectory.java
@@ -1,12 +1,12 @@
-package xal.model.probe.traj;
-
-/**
- * Abstract trajectory class for beam probes.
- * 
- * @author Craig McChesney
- * @version $id:
- * 
- */
-public abstract class BeamTrajectory extends Trajectory<ProbeState> {
-
-}
+//package xal.model.probe.traj;
+//
+///**
+// * Abstract trajectory class for beam probes.
+// * 
+// * @author Craig McChesney
+// * @version $id:
+// * 
+// */
+//public abstract class BeamTrajectory extends Trajectory<ProbeState> {
+//
+//}
--- core/src/xal/model/probe/traj/MatrixTrajectory.java
+++ core/src/xal/model/probe/traj/MatrixTrajectory.java
@@ -1,17 +1,17 @@
+////
+////  MatrixTrajectory.java
+////  xal
+////
+////  Created by Tom Pelaia on 6/21/07.
+////  Copyright 2007 Oak Ridge National Lab. All rights reserved.
+////
+//package xal.model.probe.traj;
 //
-//  MatrixTrajectory.java
-//  xal
+//import xal.tools.beam.PhaseMatrix;
 //
-//  Created by Tom Pelaia on 6/21/07.
-//  Copyright 2007 Oak Ridge National Lab. All rights reserved.
 //
-package xal.model.probe.traj;
-
-import xal.tools.beam.PhaseMatrix;
-
-
-/** interface for trajectories that can generate transfer matrices between elements */
-public interface MatrixTrajectory {
-	/** get the transfer matrix from one node to a second node */
-	public PhaseMatrix getTransferMatrix( final String fromElement, final String toElement );	
-}
+///** interface for trajectories that can generate transfer matrices between elements */
+//public interface MatrixTrajectory {
+//	/** get the transfer matrix from one node to a second node */
+//	public PhaseMatrix getTransferMatrix( final String fromElement, final String toElement );	
+//}
