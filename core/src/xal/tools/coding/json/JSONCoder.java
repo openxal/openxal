e4533b0456e20ae70de2706cf9c4063cf52adc84
Thomas Pelaia II <pelaiata@ornl.gov>
2014-09-04 13:55:12 -0400

    Typed arrays must really be typed for fidelity in encoding/decoding.

--- core/src/xal/tools/coding/json/JSONCoder.java
+++ core/src/xal/tools/coding/json/JSONCoder.java
@@ -857,7 +857,7 @@ class ArrayEncoder extends SoftValueEncoder<Object[]> {
     /** Determine whether the array is of a common extended type */
     static private boolean isTypedArray( final Object array ) {
         final Class<?> itemClass = array.getClass().getComponentType();
-		final boolean isTyped = itemClass != null && itemClass != Object.class && !JSONCoder.isStandardType( itemClass.getName() );
+		final boolean isTyped = itemClass != null && itemClass != Object.class;
 		return isTyped;
     }
 }
