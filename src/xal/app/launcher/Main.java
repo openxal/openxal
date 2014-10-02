8e18801b98a967db9670360ee662c4c6bc92c190
Thomas Pelaia II <pelaiata@ornl.gov>
2014-10-02 13:50:41 -0400

    merge changes in support of jython applications

--- src/xal/app/launcher/Main.java
+++ src/xal/app/launcher/Main.java
@@ -43,7 +43,7 @@ public class Main extends ApplicationAdaptor {
 
     
     /** Indicates whether the welcome dialog should be displayed at launch. */
-    protected boolean showsWelcomeDialogAtLaunch() {
+    public boolean showsWelcomeDialogAtLaunch() {
         return false;
     }
     
