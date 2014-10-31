3d9ae249ac86fdbda37080626f0cd0eaea0e0009
ckallen1 <allenck@ornl.gov>
2014-10-31 13:37:03 -0400

    Added debug type outs - need to switch branches to implement ring particle tracking.

--- apps/pta/src/xal/app/pta/tools/swing/NumberTextField.java
+++ apps/pta/src/xal/app/pta/tools/swing/NumberTextField.java
@@ -252,7 +252,7 @@ public class NumberTextField extends JTextField {
             if(this.cmpParent.isEditable()) { 
                 this.cmpParent.updateDisplay();
                 this.cmpParent.setBackground( getBkgndNormalColor() );
-                this.cmpParent.postActionEvent();
+//                this.cmpParent.postActionEvent();
             }
         } 
     }
@@ -624,6 +624,10 @@ public class NumberTextField extends JTextField {
         try{
             String  strInput = this.getText();
             Number  numInput = this.fmtDispl.parse(strInput);
+            
+            System.out.println("NumberTextField#updateDisplay: numInput = " + numInput);
+            System.out.println();
+            
 //            String  strDispl = this.fmtDispl.format(numInput);
 
             // TODO: Clean this up
