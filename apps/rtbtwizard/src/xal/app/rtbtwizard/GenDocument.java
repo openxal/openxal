1fbd19dcc8730dc957b4bcc59e1961c35df82cd0
Thomas Pelaia II <pelaiata@ornl.gov>
2015-02-23 11:29:18 -0500

    Reference the harp channel handles by the constants.

--- apps/rtbtwizard/src/xal/app/rtbtwizard/GenDocument.java
+++ apps/rtbtwizard/src/xal/app/rtbtwizard/GenDocument.java
@@ -224,9 +224,8 @@ public class GenDocument extends AcceleratorDocument implements DataListener{
 		createAgents(rtbtseq);
 		//nbpmagents=rtbtagents.size();
 		harp = rtbtseq.getNodesOfType( "Harp" ).get(0);
-		//TODO: now that there is a WireHarp class, we really need to get the handles from a constant instead of hard coding
-		harpxch=harp.getChannel("FitAttrHorStd");
-		harpych=harp.getChannel("FitAttrVerStd");
+		harpxch=harp.getChannel( WireHarp.FitAttrSet.X_RMS_HANDLE );
+		harpych=harp.getChannel( WireHarp.FitAttrSet.Y_RMS_HANDLE );
 		harpxch.requestConnection();
 		harpych.requestConnection();
 		repratech = accl.getTimingCenter().getChannel( TimingCenter.REP_RATE_HANDLE );
