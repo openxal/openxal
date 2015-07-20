fdb54791394b1db3544b0f321ad29e14439038af
Yngve Inntjore Levinsen <Yngve.Levinsen@esss.se>
2015-07-20 09:40:45 +0200

    [apps] #57 replaced <br/> -> <br>  according to javadoc error/warnings  Java 8r45  checked that it compiles with ant on OSX

--- apps/virtualaccelerator/src/xal/app/virtualaccelerator/VADocument.java
+++ apps/virtualaccelerator/src/xal/app/virtualaccelerator/VADocument.java
@@ -68,8 +68,8 @@ import xal.tools.dispatch.*;
  * is created in the case of a ring.  The method <code>TransferMapState#setPhaseCoordinates</code>
  * is called to create an initial offset.  This does nothing because transfer map probes
  * do not have phase coordinates any longer, the method is deprecated.
- * <br/>
- * <br/>
+ * <br>
+ * <br>
  * - The offset for the above call is hard coded.  As are many features in this class.
  * </p>
  * 
