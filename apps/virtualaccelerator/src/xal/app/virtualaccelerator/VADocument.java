b7ee7800c0b11e94522115251e2b2a79e91d5ce9
Yngve Inntjore Levinsen <yngve.levinsen@gmail.com>
2015-07-22 14:05:33 +0200

    [apps] #57  header used out of sequence: <H4>   moved out of paragraphs, replaced with h3

--- apps/virtualaccelerator/src/xal/app/virtualaccelerator/VADocument.java
+++ apps/virtualaccelerator/src/xal/app/virtualaccelerator/VADocument.java
@@ -62,8 +62,8 @@ import xal.tools.dispatch.*;
 //TODO: CKA - Many unused imports
 
 /**
+ * <h3>CKA NOTES:</h3>
  * <p>
- * <h4>CKA NOTES:</h4>
  * - In method <code>{@link #createDefaultProbe()}</code> a <code>TransferMapProbe</code>
  * is created in the case of a ring.  The method <code>TransferMapState#setPhaseCoordinates</code>
  * is called to create an initial offset.  This does nothing because transfer map probes
