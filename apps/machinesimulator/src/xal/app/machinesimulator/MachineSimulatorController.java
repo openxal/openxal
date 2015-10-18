dcf6f8970d7e198d262b72ce423efb540683c050
luxiaohan <luxh@ihep.ac.cn>
2015-10-18 19:25:29 +0800

    change getParameterData() method

--- apps/machinesimulator/src/xal/app/machinesimulator/MachineSimulatorController.java
+++ apps/machinesimulator/src/xal/app/machinesimulator/MachineSimulatorController.java
@@ -6,6 +6,7 @@ package xal.app.machinesimulator;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javax.swing.JButton;
@@ -16,6 +17,7 @@ import javax.swing.JTextField;
 import xal.extension.bricks.WindowReference;
 import xal.extension.widgets.plot.FunctionGraphsJPanel;
 import xal.extension.widgets.swing.KeyValueFilteredTableModel;
+import xal.tools.data.KeyValueAdaptor;
 
 /**
  * @author luxiaohan
@@ -30,6 +32,7 @@ public class MachineSimulatorController {
     final KeyValueFilteredTableModel<MachineSimulationRecord> STATES_TABLE_MODEL;
      /** main model */
      final MachineModel MODEL;
+     public List<MachineSimulationRecord> _allRecords;
       
      /**result of MachineSimulatoion */
      public MachineSimulation _MachineSimulation;
@@ -37,6 +40,10 @@ public class MachineSimulatorController {
      final private MachineSimulatorDocument _Document;
      /** the plotter*/
      public MachineSimulatorPlotter _MachineSimulatorPlotter;
+       
+     final KeyValueAdaptor KEY_VALUE_ADAPTOR;
+     
+     final HashMap<String, List<Double>> PLOT_DATA;
        /** initialize the data list of parameters*/
      public List<Double> _position=new ArrayList<Double>();
      public List<Double> kineticenery=new ArrayList<Double>();     
@@ -66,12 +73,13 @@ public class MachineSimulatorController {
 		_Document=document;
 		WINDOW_REFERENCE=windowReference;
 		STATES_TABLE_MODEL = new KeyValueFilteredTableModel<MachineSimulationRecord>();
+		PLOT_DATA = new HashMap<String,List<Double>>();
+		KEY_VALUE_ADAPTOR= new KeyValueAdaptor();
         // initialize the model here
         MODEL = _Document.getModel();
 
 //        RECORDS=_MachineSimulation.getSimulationRecords();
          configureTableWindow(WINDOW_REFERENCE);
-         configurePlotWindow(WINDOW_REFERENCE);
 	}
 	
 	
@@ -107,7 +115,9 @@ public class MachineSimulatorController {
         STATES_TABLE_MODEL.setInputFilterComponent( statesTableFilterField );
         STATES_TABLE_MODEL.setMatchingKeyPaths( "elementID" );
         
-        
+        final FunctionGraphsJPanel twissParametersplot = (FunctionGraphsJPanel) windowReference.getView("States Plot");
+        _MachineSimulatorPlotter=new MachineSimulatorPlotter(twissParametersplot);
+
         // handle the parameter selections of Table view
         final JCheckBox kineticEnergyCheckbox = (JCheckBox)windowReference.getView( "Kinetic Energy Checkbox" );
         
@@ -177,6 +187,10 @@ public class MachineSimulatorController {
                 // append vector parameters after scalar parameters
                 System.arraycopy( vectorParameterKeys, 0, parameterKeys, scalarParameterKeys.length + standardParameterKeys.length, vectorParameterKeys.length );
                 STATES_TABLE_MODEL.setKeyPaths( parameterKeys );
+                
+                final String[] parameterKeysForPlot=new String[parameterKeys.length-1];
+                System.arraycopy(parameterKeys, 1, parameterKeysForPlot, 0, parameterKeys.length-1);
+                getParametersData(_allRecords, parameterKeysForPlot);
             }
         };
         
@@ -195,7 +209,7 @@ public class MachineSimulatorController {
         betatronPhaseCheckbox.addActionListener( PARAMETER_HANDLER );
         
         // perform the initial parameter display configuration
-        PARAMETER_HANDLER.actionPerformed( null );
+       // PARAMETER_HANDLER.actionPerformed( null );
         
         
         // configure the run button
@@ -204,7 +218,7 @@ public class MachineSimulatorController {
             public void actionPerformed( final ActionEvent event ) {
                 System.out.println( "running the model..." );
                 final MachineSimulation simulation = MODEL.runSimulation();
-                getParametersData(simulation);
+                _allRecords=simulation.getSimulationRecords();
                 STATES_TABLE_MODEL.setRecords( simulation.getSimulationRecords() );
             }
         });
@@ -219,160 +233,21 @@ public class MachineSimulatorController {
 		});
     }
     
-    /**configure the plot window*/ 
-    private void configurePlotWindow(final WindowReference windowReference){
-        final FunctionGraphsJPanel twissParametersplot = (FunctionGraphsJPanel) WINDOW_REFERENCE.getView("States Plot");
-        _MachineSimulatorPlotter=new MachineSimulatorPlotter(twissParametersplot);
-
-        // handle the parameter selections of plot view
-        final JCheckBox kineticEnergyCheckbox = (JCheckBox)windowReference.getView( "Kinetic Energy Checkbox1" );
-        final JCheckBox xSelectionCheckbox = (JCheckBox)windowReference.getView( "X Selection Checkbox1" );
-        final JCheckBox ySelectionCheckbox = (JCheckBox)windowReference.getView( "Y Selection Checkbox1" );
-        final JCheckBox zSelectionCheckbox = (JCheckBox)windowReference.getView( "Z Selection Checkbox1" );
-        final JCheckBox betaCheckbox = (JCheckBox)windowReference.getView( "Beta Checkbox1" );
-        final JCheckBox alphaCheckbox = (JCheckBox)windowReference.getView( "Alpha Checkbox1" );
-        final JCheckBox gammaCheckbox = (JCheckBox)windowReference.getView( "Gamma Checkbox1" );
-        final JCheckBox emittanceCheckbox = (JCheckBox)windowReference.getView( "Emittance Checkbox1" );
-        final JCheckBox beamSizeCheckbox = (JCheckBox)windowReference.getView( "Beam Size Checkbox1" );
-        final JCheckBox betatronPhaseCheckbox = (JCheckBox)windowReference.getView( "Betatron Phase Checkbox1" );
-
-        final ActionListener PARAMETER_HANDLER = new ActionListener() {
-            public void actionPerformed( final ActionEvent event ) {
-           	 twissParametersplot.removeAllGraphData();
-            	   _MachineSimulatorPlotter.setupPlot(twissParametersplot);
-              final int[] planes = new int[3]; 
-            	if(xSelectionCheckbox.isSelected())
-            		planes[0]=1;
-            	else planes[0]=0;
-            	if(ySelectionCheckbox.isSelected())
-            		planes[1]=1;
-            	else planes[1]=0;
-            	if(zSelectionCheckbox.isSelected())
-            		planes[2]=1;
-            	else planes[2]=0;
-            	    if(kineticEnergyCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, kineticenery, "kineticenery");
-            	if(planes[0]==1){
-            		if(betaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, betax, "betax");
-            		if(alphaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, alphax, "alphax");
-            		if(gammaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, gammax, "gammax");
-            		if(emittanceCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, emittancex, "emittancex");
-            		if(beamSizeCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, beamsizex, "beamsizex");
-            		if(betatronPhaseCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, betatronphasex, "betatronphasex");
-            	}
-            	if(planes[1]==1){
-            		if(betaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, betay, "betay");
-            		if(alphaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, alphay, "alphay");
-            		if(gammaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, gammay, "gammay");
-            		if(emittanceCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, emittancey, "emittancey");
-            		if(beamSizeCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, beamsizey, "beamsizey");
-            		if(betatronPhaseCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, betatronphasey, "betatronphasey");
-            	}
-            	if(planes[2]==1){
-            		if(betaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, betaz, "betaz");
-            		if(alphaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, alphaz, "alphaz");
-            		if(gammaCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, gammaz, "gammaz");
-            		if(emittanceCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, emittancez, "emittancez");
-            		if(beamSizeCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, beamsizez, "beamsizez");
-            		if(betatronPhaseCheckbox.isSelected())_MachineSimulatorPlotter.showtwissplot(_position, betatronphasez, "betatronphasez");
-            	}
-            
-            	
-            }
-        };
- 	  
-        kineticEnergyCheckbox.addActionListener( PARAMETER_HANDLER );
-        xSelectionCheckbox.addActionListener( PARAMETER_HANDLER );
-        ySelectionCheckbox.addActionListener( PARAMETER_HANDLER );
-        zSelectionCheckbox.addActionListener( PARAMETER_HANDLER );
-        betaCheckbox.addActionListener( PARAMETER_HANDLER );
-        alphaCheckbox.addActionListener( PARAMETER_HANDLER );
-        gammaCheckbox.addActionListener( PARAMETER_HANDLER );
-        emittanceCheckbox.addActionListener( PARAMETER_HANDLER );
-        beamSizeCheckbox.addActionListener( PARAMETER_HANDLER );
-        betatronPhaseCheckbox.addActionListener( PARAMETER_HANDLER );
-        
-        // perform the initial parameter display configuration
-        PARAMETER_HANDLER.actionPerformed( null );
-        
-        // configure the Clear All button
-        final JButton ClearButton = (JButton)windowReference.getView( "Clear All" );
-        ClearButton.addActionListener( new ActionListener() {
-            public void actionPerformed( final ActionEvent event ) {
-            	 kineticEnergyCheckbox.setSelected(false);
-                xSelectionCheckbox.setSelected(false);
-                ySelectionCheckbox.setSelected(false);
-                zSelectionCheckbox.setSelected(false);
-                betaCheckbox.setSelected(false);
-                alphaCheckbox.setSelected(false);
-                gammaCheckbox.setSelected(false);
-                emittanceCheckbox.setSelected(false);
-                beamSizeCheckbox.setSelected(false);
-                betatronPhaseCheckbox.setSelected(false);
-                
-                PARAMETER_HANDLER.actionPerformed( null );              
-            }
-        });
-        
+    /** get the parameter data from Machinesimulation*/  
+    public void getParametersData(List<MachineSimulationRecord> records,String[] keyPaths){ 
+    	PLOT_DATA.clear();
+    	for(final String keyPath:keyPaths){
+    		PLOT_DATA.put(keyPath, new ArrayList<Double>(records.size()));
+    		for(final MachineSimulationRecord record:records){
+    			PLOT_DATA.get(keyPath).add((Double)KEY_VALUE_ADAPTOR.valueForKeyPath(record,keyPath));    			
+    		}
+    	}
+    	
+    	
     }
-  /** get the parameter data from Machinesimulation*/  
-    public void getParametersData(MachineSimulation simulation){
-    	_MachineSimulation=simulation;
-      _position=_MachineSimulation.getAllPosition();
-        kineticenery.removeAll(kineticenery);
-        
-        betax.removeAll(betax);
-        betay.removeAll(betay);
-        betaz.removeAll(betaz);
-        
-        alphax.removeAll(alphax);
-        alphay.removeAll(alphay);
-        alphaz.removeAll(alphaz);
-        
-        gammax.removeAll(gammax);
-        gammay.removeAll(gammay);
-        gammaz.removeAll(gammaz);
-        
-        emittancex.removeAll(emittancex);
-        emittancey.removeAll(emittancey);
-        emittancez.removeAll(emittancez);
-        
-        beamsizex.removeAll(beamsizex);
-        beamsizey.removeAll(beamsizey);
-        beamsizez.removeAll(beamsizez);
-        
-        betatronphasex.removeAll(betatronphasex);
-        betatronphasey.removeAll(betatronphasey);
-        betatronphasez.removeAll(betatronphasez);
-      for(int i=0;i<_MachineSimulation.getSimulationRecords().size();i++){
-    	  kineticenery.add(_MachineSimulation.getSimulationRecords().get(i).getProbeState().getKineticEnergy());
-    	  
-        betax.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[0].getBeta());  
-    	  betay.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[1].getBeta());
-    	  betaz.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[2].getBeta());
-    	  
-        alphax.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[0].getAlpha());  
-        alphay.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[1].getAlpha());
-        alphaz.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[2].getAlpha());
-        
-        gammax.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[0].getGamma());  
-    	  gammay.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[1].getGamma());
-    	  gammaz.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[2].getGamma());
-    	  
-        emittancex.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[0].getEmittance());  
-        emittancey.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[1].getEmittance());
-        emittancez.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[2].getEmittance());
-        
-        beamsizex.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[0].getEnvelopeRadius());  
-        beamsizey.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[1].getEnvelopeRadius());
-        beamsizez.add(_MachineSimulation.getSimulationRecords().get(i).getTwissParameters()[2].getEnvelopeRadius());
-        
-        betatronphasex.add(_MachineSimulation.getSimulationRecords().get(i).getBetatronPhase().getx());  
-        betatronphasey.add(_MachineSimulation.getSimulationRecords().get(i).getBetatronPhase().gety());
-        betatronphasez.add(_MachineSimulation.getSimulationRecords().get(i).getBetatronPhase().getz());
-      }
-      
-          
-    }
+    
+    
+
 }
 
 
